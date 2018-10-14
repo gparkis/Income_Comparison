3c4886fdd55f66eee3baef095ebbc2ca09c25fde
Grant Parkis <gparkis@gmail.com>
2018-10-14 19:09:46 -0400

    Start of Income comparison

---
+++ Income Comparison/Income Comparison/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Income_Comparison
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("     *******************");
+            Console.WriteLine("");
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("");
+            Console.WriteLine("     *******************");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Person 1");
+            Console.Write("   Please enter your hourly rate: $");
+            double hourlyRate = Convert.ToDouble(Console.ReadLine());
+            Console.Write("   Please enter your weekly hours worked: ");
+            double weeklyHoursWorked = Convert.ToDouble(Console.ReadLine());
+         
+
+
+
+            Console.Read();
+        }
+    }
+}
