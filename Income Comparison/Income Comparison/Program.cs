4c8ee4b2f41bef990777ac0cc356c8455ac85412
Grant Parkis <gparkis@gmail.com>
2018-10-14 19:19:45 -0400

    final code

--- Income Comparison/Income Comparison/Program.cs
+++ Income Comparison/Income Comparison/Program.cs
@@ -19,12 +19,39 @@ namespace Income_Comparison
             Console.WriteLine("");
             Console.WriteLine("Person 1");
             Console.Write("   Please enter your hourly rate: $");
-            double hourlyRate = Convert.ToDouble(Console.ReadLine());
+            double hourlyRate1 = Convert.ToDouble(Console.ReadLine());
             Console.Write("   Please enter your weekly hours worked: ");
-            double weeklyHoursWorked = Convert.ToDouble(Console.ReadLine());
-         
+            double weeklyHoursWorked1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("     *******************");
+            Console.WriteLine("");
+            Console.WriteLine("Person 2");
+            Console.Write("   Please enter your hourly rate: $");
+            double hourlyRate2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("   Please enter your weekly hours worked: ");
+            double weeklyHoursWorked2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("     *******************");
+            Console.WriteLine("");
 
+            Console.WriteLine("     PERSON 1: Annual Salary ");
+            Console.WriteLine("_________________________________");
+            Console.WriteLine("");
+            Console.WriteLine("     $" + hourlyRate1 * weeklyHoursWorked1 * 52);
+            Console.WriteLine("");
+            Console.WriteLine("     *******************");
+            Console.WriteLine("");
+            Console.WriteLine("     PERSON 2: Annual Salary ");
+            Console.WriteLine("_________________________________");
+            Console.WriteLine("");
+            Console.WriteLine("     $" + hourlyRate2 * weeklyHoursWorked2 * 52);
+            Console.WriteLine("");
+            Console.WriteLine("     *******************");
+            Console.WriteLine("");
 
+            bool person1MakeMore = (hourlyRate1 * weeklyHoursWorked1) > (hourlyRate2 * weeklyHoursWorked2);
+            Console.WriteLine("Does Person 1 make more money than Person 2? ");
+            Console.WriteLine("   " + person1MakeMore);
 
             Console.Read();
         }
